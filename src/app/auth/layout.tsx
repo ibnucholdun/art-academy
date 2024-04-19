a086ca01e0ec1f910e9f11ec7bb6a5a9ce49311b
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-19 10:27:59 +0700

    feat(auth/register): confirm email verification

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = { children: React.ReactNode };
 
 const AuthLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#FDFAF3] to-[#6f3823]">
-      {children}
-    </div>
+    <Suspense>
+      <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#FDFAF3] to-[#6f3823]">
+        {children}
+      </div>
+    </Suspense>
   );
 };
 
