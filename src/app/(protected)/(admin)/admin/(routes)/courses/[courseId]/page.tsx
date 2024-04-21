f6b2eb52c42b48cc0b6d873c1ac0660387abdfcc
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-21 11:51:45 +0700

    feat(admin/courses/courseId): add course edit page UI

--- src/app/(protected)/(admin)/admin/(routes)/courses/[courseId]/page.tsx
+++ src/app/(protected)/(admin)/admin/(routes)/courses/[courseId]/page.tsx
@@ -1,3 +1,8 @@
+import { IconBadge } from "@/components/IconBadge";
+import { currentUser } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
@@ -6,8 +11,56 @@ type Props = {
   };
 };
 
-const CourseIdPage: React.FC<Props> = ({ params }) => {
-  return <div>CourseIdPage {params.courseId}</div>;
+const CourseIdPage: React.FC<Props> = async ({ params }) => {
+  const user = await currentUser();
+
+  if (!user?.id || user?.role !== "ADMIN") {
+    return redirect("/");
+  }
+
+  const course = await prisma.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="">
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
