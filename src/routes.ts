597e22224a116f09c549ecc07253a65b754e979b
ibnucholdun <ibnucholdun023@gmail.com>
2024-05-10 14:33:14 +0700

    feat(user/courses/coursesId): add stripe integration

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,12 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/", "/auth/new-verification", "/api/uploadthing"];
+export const publicRoutes = [
+  "/",
+  "/auth/new-verification",
+  "/api/uploadthing",
+  "/api/webhook",
+];
 
 /**
  * An array of routes that are used for authentication
