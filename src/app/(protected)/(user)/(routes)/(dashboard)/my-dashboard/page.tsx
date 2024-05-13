888bd0a1e6dde84bb858e093381aeb7d8c57d7bf
ibnucholdun <ibnucholdun023@gmail.com>
2024-05-13 10:43:38 +0700

    feat(user/dashboard): add layout user dashboard

---
+++ src/app/(protected)/(user)/(routes)/(dashboard)/my-dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const MyDashboardPage = (props: Props) => {
+  return <div>MyDashboardPage</div>;
+};
+
+export default MyDashboardPage;
--- src/components/auth/UserButton.tsx
+++ src/components/auth/UserButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   DropdownMenu,
@@ -6,14 +8,17 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { LogOut, User } from "lucide-react";
+import { Home, LayoutDashboard, LogOut, User } from "lucide-react";
 import LogoutButton from "./LogoutButton";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type Props = {
   user: any;
 };
 
 const UserButton: React.FC<Props> = ({ user }) => {
+  const pathname = usePathname();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -25,10 +30,36 @@ const UserButton: React.FC<Props> = ({ user }) => {
           )}
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40 bg-brown300" align="end">
+      <DropdownMenuContent className="w-40" align="end">
         <DropdownMenuItem className="hover:bg-white">
+          <div className="flex items-center justify-center gap-x-2">
+            <Avatar>
+              {user.image === null ? (
+                <AvatarFallback>{user?.name[0].toUpperCase()}</AvatarFallback>
+              ) : (
+                <AvatarImage src={user?.image} />
+              )}
+            </Avatar>
+            <p className="">{user?.name}</p>
+          </div>
+        </DropdownMenuItem>
+        <hr className="my-2 h-px border-0 bg-slate-300" />
+        <DropdownMenuItem className="hover:bg-white">
+          {pathname === "/my-dashboard" ? (
+            <>
+              <Home className="mr-2 h-4 w-4" />
+              <Link href="/">Back to Home</Link>
+            </>
+          ) : (
+            <>
+              <LayoutDashboard className="mr-2 h-4 w-4" />
+              <Link href="/my-dashboard">Dashboard</Link>
+            </>
+          )}
+        </DropdownMenuItem>
+        <DropdownMenuItem className="hover:bg-white ">
           <User className="mr-2 h-4 w-4" />
-          Profile
+          <Link href="/profile">Profile</Link>
         </DropdownMenuItem>
         <LogoutButton>
           <DropdownMenuItem className="hover:bg-white">
