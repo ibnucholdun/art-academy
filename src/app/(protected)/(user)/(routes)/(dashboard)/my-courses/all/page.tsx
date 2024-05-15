407c58e7563f84b8b59100dbcc13ac00ad429165
ibnucholdun <ibnucholdun023@gmail.com>
2024-05-15 22:35:27 +0700

    fix(user/dashboard/my-courses): add no items in my-courses

--- src/app/(protected)/(user)/(routes)/(dashboard)/my-courses/all/page.tsx
+++ src/app/(protected)/(user)/(routes)/(dashboard)/my-courses/all/page.tsx
@@ -2,6 +2,7 @@ import getCourses from "@/actions/getCourses";
 import { currentUser } from "@/lib/auth";
 import React from "react";
 import CourseCard from "../../my-dashboard/_components/CourseCard";
+import NoItems from "@/components/NoItem";
 
 type Props = {};
 
@@ -9,11 +10,21 @@ const MyCoursesAllPage = async (props: Props) => {
   const user = await currentUser();
   const courses = await getCourses({ userId: user?.id! });
 
+  const filteredCourses = courses.filter(
+    (course) => course.purchases.length !== 0
+  );
+
   return (
     <>
-      {courses.map((course) => (
-        <CourseCard key={course.id} course={course} />
-      ))}
+      {filteredCourses.length === 0 ? (
+        <NoItems title="Sorry no courses found..." description="" />
+      ) : (
+        <>
+          {filteredCourses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </>
+      )}
     </>
   );
 };
--- src/app/(protected)/(user)/(routes)/(dashboard)/my-courses/in-progress/page.tsx
+++ src/app/(protected)/(user)/(routes)/(dashboard)/my-courses/in-progress/page.tsx
@@ -2,6 +2,7 @@ import getCourses from "@/actions/getCourses";
 import { currentUser } from "@/lib/auth";
 import React from "react";
 import CourseCard from "../../my-dashboard/_components/CourseCard";
+import NoItems from "@/components/NoItem";
 
 type Props = {};
 
@@ -14,9 +15,15 @@ const MyCoursesInProgressPage = async (props: Props) => {
 
   return (
     <>
-      {coursesInProgress.map((course) => (
-        <CourseCard key={course.id} course={course} />
-      ))}
+      {coursesInProgress.length === 0 ? (
+        <NoItems title="Sorry no courses found..." description="" />
+      ) : (
+        <>
+          {coursesInProgress.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </>
+      )}
     </>
   );
 };
