2b7084f8a45e8e7a51f8508ed316ef3c52f4bbf5
ibnucholdun <ibnucholdun023@gmail.com>
2024-05-15 13:09:35 +0700

    feat(user/profile/account): add protected not available OAuth

--- src/app/(protected)/(user)/(routes)/profile/account/_components/EmailForm.tsx
+++ src/app/(protected)/(user)/(routes)/profile/account/_components/EmailForm.tsx
@@ -19,6 +19,7 @@ import FormError from "@/components/FormError";
 import FormSuccess from "@/components/FormSuccess";
 import { Button } from "@/components/ui/button";
 import { changeEmail } from "@/actions/changeEmail";
+import { TriangleAlert } from "lucide-react";
 
 type Props = {
   user: any;
@@ -61,36 +62,51 @@ const EmailForm = ({ user }: Props) => {
         <hr className="mt-4 h-1 w-full" />
       </CardHeader>
       <CardContent>
-        <Form {...form}>
-          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="space-y-4">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>New Email</FormLabel>
-                    <FormControl>
-                      <>
-                        <Input type="email" disabled={isPending} {...field} />
-                        <p className="text-xs text-muted-foreground">
-                          The email will change when you press the verification
-                          link sent to your new email.
-                        </p>
-                      </>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+        {user.isOAuth === false && (
+          <Form {...form}>
+            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>New Email</FormLabel>
+                      <FormControl>
+                        <>
+                          <Input type="email" disabled={isPending} {...field} />
+                          <p className="text-xs text-muted-foreground">
+                            The email will change when you press the
+                            verification link sent to your new email.
+                          </p>
+                        </>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {error && <FormError message={error} />}
+              {success && <FormSuccess message={success} />}
+              <Button type="submit" disabled={isPending}>
+                Change Email
+              </Button>
+            </form>
+          </Form>
+        )}
+        {user.isOAuth && (
+          <div className="flex min-h-[200px] items-center justify-center flex-col rounded-md border border-dashed p-7 text-center animate-in fade-in-50">
+            <div className="flex h-20 w-20 items-center justify-center rounded-full bg-orange-500/10">
+              <TriangleAlert className="h-10 w-10 text-primary text-orange-500" />
             </div>
-            {error && <FormError message={error} />}
-            {success && <FormSuccess message={success} />}
-            <Button type="submit" disabled={isPending}>
-              Change Email
-            </Button>
-          </form>
-        </Form>
+            <h2 className="mt-6 text-xl font-semibold text-orange-500">
+              Not available
+            </h2>
+            <p className="text-muted-foreground mt-2 text-center text-sm leading-6 ">
+              This feature is not available for OAuth users
+            </p>
+          </div>
+        )}
       </CardContent>
     </Card>
   );
--- src/app/(protected)/(user)/(routes)/profile/account/_components/PasswordForm.tsx
+++ src/app/(protected)/(user)/(routes)/profile/account/_components/PasswordForm.tsx
@@ -18,7 +18,7 @@ import { Input } from "@/components/ui/input";
 import FormError from "@/components/FormError";
 import FormSuccess from "@/components/FormSuccess";
 import { Button } from "@/components/ui/button";
-import { EyeIcon, EyeOff } from "lucide-react";
+import { EyeIcon, EyeOff, FileWarning, TriangleAlert } from "lucide-react";
 import { changePassword } from "@/actions/changePassword";
 
 type Props = {
@@ -64,79 +64,94 @@ const PasswordForm = ({ user }: Props) => {
         <hr className="mt-4 h-1 w-full" />
       </CardHeader>
       <CardContent>
-        <Form {...form}>
-          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="space-y-4">
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <div className="relative">
-                        <Input
-                          type={visible ? "text" : "password"}
-                          {...field}
-                          placeholder="Enter the old password"
-                          disabled={isPending}
-                        />
-                        <Button
-                          type="button"
-                          className="absolute right-3 top-1/2 transform -translate-y-1/2 p-0"
-                          variant="link"
-                          onClick={() => setVisible(!visible)}>
-                          {visible ? (
-                            <EyeOff className="h-5 w-5 text-gray-700" />
-                          ) : (
-                            <EyeIcon className="h-5 w-5 text-gray-700" />
-                          )}
-                        </Button>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="newPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>New Password</FormLabel>
-                    <FormControl>
-                      <div className="relative">
-                        <Input
-                          type={visible ? "text" : "password"}
-                          {...field}
-                          placeholder="Enter the new password"
-                          disabled={isPending}
-                        />
-                        <Button
-                          type="button"
-                          className="absolute right-3 top-1/2 transform -translate-y-1/2 p-0"
-                          variant="link"
-                          onClick={() => setVisible(!visible)}>
-                          {visible ? (
-                            <EyeOff className="h-5 w-5 text-gray-700" />
-                          ) : (
-                            <EyeIcon className="h-5 w-5 text-gray-700" />
-                          )}
-                        </Button>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+        {user.isOAuth === false && (
+          <Form {...form}>
+            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type={visible ? "text" : "password"}
+                            {...field}
+                            placeholder="Enter the old password"
+                            disabled={isPending}
+                          />
+                          <Button
+                            type="button"
+                            className="absolute right-3 top-1/2 transform -translate-y-1/2 p-0"
+                            variant="link"
+                            onClick={() => setVisible(!visible)}>
+                            {visible ? (
+                              <EyeOff className="h-5 w-5 text-gray-700" />
+                            ) : (
+                              <EyeIcon className="h-5 w-5 text-gray-700" />
+                            )}
+                          </Button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="newPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>New Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type={visible ? "text" : "password"}
+                            {...field}
+                            placeholder="Enter the new password"
+                            disabled={isPending}
+                          />
+                          <Button
+                            type="button"
+                            className="absolute right-3 top-1/2 transform -translate-y-1/2 p-0"
+                            variant="link"
+                            onClick={() => setVisible(!visible)}>
+                            {visible ? (
+                              <EyeOff className="h-5 w-5 text-gray-700" />
+                            ) : (
+                              <EyeIcon className="h-5 w-5 text-gray-700" />
+                            )}
+                          </Button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {error && <FormError message={error} />}
+              {success && <FormSuccess message={success} />}
+              <Button type="submit" disabled={isPending}>
+                Change Password
+              </Button>
+            </form>
+          </Form>
+        )}
+        {user.isOAuth && (
+          <div className="flex min-h-[200px] items-center justify-center flex-col rounded-md border border-dashed p-7 text-center animate-in fade-in-50">
+            <div className="flex h-20 w-20 items-center justify-center rounded-full bg-orange-500/10">
+              <TriangleAlert className="h-10 w-10 text-primary text-orange-500" />
             </div>
-            {error && <FormError message={error} />}
-            {success && <FormSuccess message={success} />}
-            <Button type="submit" disabled={isPending}>
-              Change Password
-            </Button>
-          </form>
-        </Form>
+            <h2 className="mt-6 text-xl font-semibold text-orange-500">
+              Not available
+            </h2>
+            <p className="text-muted-foreground mt-2 text-center text-sm leading-6 ">
+              This feature is not available for OAuth users
+            </p>
+          </div>
+        )}
       </CardContent>
     </Card>
   );
